6e880601620108b1c5d1716f08b5e62510ade1b1
sedian27 <diestrasergio@gmail.com>
2022-01-25 16:15:15 -0500

    Agregue segunda opcion para el reto de hoy

--- assets/practicas/practica04.js
+++ assets/practicas/practica04.js
@@ -8,6 +8,7 @@ let h3Ciudades = document.getElementById("ciudades");
 
 let ciudades = [];
 
+// Ingresar 6 ciudades sin tanta validacion
 const agregarCiudades = () => {
   if (ciudades.length < 6) {
     for (let i = 0; i < 6; i++) {
@@ -16,6 +17,18 @@ const agregarCiudades = () => {
     }
   } else {
     alert("Las 6 ciudades ya fueron ingresadas!");
+  }
+};
+
+// Agregar la cantidad de ciudades que se quieran sin que se repitan y no dejando que se ingresen campos vacios o nulos
+const agregarCiudadesValidadas = () => {
+  let ciudad = prompt("Ingrese nombre de la ciudad:");
+  if (ciudad == "" || ciudad == null) {
+    alert("Error al ingresar la ciudad");
+  } else if (ciudades.indexOf(ciudad) > -1) {
+    alert("La ciudad ya fue ingresada!");
+  } else {
+    ciudades.push(ciudad);
   }
 };
 
